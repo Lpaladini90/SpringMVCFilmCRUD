e2058a3d6e68dca740d7b2af8de11e555a8ebac1
Lpaladini90 <lpaladini@me.com>
2022-04-09 10:10:02 -0700

    Film Controller Update

--- src/main/java/com/skilldistillery/mvcfilmsite/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/mvcfilmsite/controllers/FilmController.java
@@ -1,10 +1,15 @@
 package com.skilldistillery.mvcfilmsite.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.mvcfilmsite.data.FilmDAO;
+import com.skilldistillery.mvcfilmsite.entities.Film;
 
 @Controller
 public class FilmController {
@@ -15,7 +20,45 @@ public class FilmController {
 	@RequestMapping(path = {"/", "home.do"})
 	public String home() {
 		return "WEB-INF/home.jsp";
+		
+		
 	}
+	    
+	
+	@RequestMapping(path = "findFilmById.do", params = "filmid", method = RequestMethod.GET)
+	public ModelAndView findFilmById(int id) {
+		ModelAndView mv = new ModelAndView();
+		Film f= filmDao.findFilmById(id);
+		mv.addObject("filmid", f);
+		mv.setViewName("WEB-INF/searchById.jsp");
+	
+		return mv;
+	}
+	
+	@RequestMapping(path = "findFilmBySearch.do", params = "filmsearch", method = RequestMethod.GET)
+	public ModelAndView findFilmBySearch(String choice) {
+		ModelAndView mv = new ModelAndView();
+		List<Film> films = filmDao.findFilmBySearch(choice);
+		mv.addObject("filmsearch", films);
+		mv.setViewName("WEB-INF/searchByKeyword.jsp");
+		return mv;
+	}
+	
+	
+	@RequestMapping(path = "createFilm.do", params = "createfilm", method = RequestMethod.POST)
+	public ModelAndView creatFilm(Film film) {
+		ModelAndView mv = new ModelAndView();
+		Film newFilm= filmDao.createFilm(film);
+		mv.addObject("createfilm", newFilm);
+		mv.setViewName("WEB-INF/createFilm.jsp");
+		
+		return mv;
+		
+	}
+	
+//	@RequestMapping(path = "editfilm", params = "editFilm", method = RequestMethod.POST)
+//	public ModelAndView editFilm()
+	
 	
 	
 }
